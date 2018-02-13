441b17b2702f50324fea4b6c0efb737d752f97ee
Taif <taifamer456@gmail.com>
2018-02-13 20:14:14 +0100

    added another cleaning script & aik linux

--- build_A8_kernel.sh
+++ build_A8_kernel.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 #
+# Swift Kernel Build Script 
+# Coded by BlackMesa/TaifAljaloo/AnanJaser @2018
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 echo " ....building Swift kernel started ...."
 # Init Fields
 SW_DIR=/home/taif/Desktop/kernel/kernel
@@ -69,4 +84,10 @@ echo "******************buidling DTB is done***********************"
 echo "****************************************************************"
 echo "****************************************************************"
 echo "****************************************************************"
+
+# remove some useless files
+cd /home/taif/Desktop/kernel/kernel/sw-tools/AIK-Linux
+
+sudo ./cleanup.sh
+
 	
